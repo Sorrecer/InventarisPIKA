25fdf36eb422133b68e208af01253117902020df
Sorrecer <Anagakenny24@gmail.com>
2022-03-14 17:25:02 +0700

    data barang

--- app/Controllers/Barang.php
+++ app/Controllers/Barang.php
@@ -10,18 +10,33 @@ class Barang extends BaseController
 {
     public function index()
     {
-        $BarangMasukModel = new BarangMasukModel();
-        $BarangMasukModel->builder();
-        $hasil = $BarangMasukModel->builder()->select('barang.nama_barang, kategori.nama_kategori,
-        (sum(barang_masuk.jumlah_barang) - sum(barang_keluar.jumlah_barang)div count(barang_masuk.id_transaksi)) AS Jumlah, ruang.nama_ruang')
-            ->join('barang_keluar', 'barang_masuk.id_barang = barang_keluar.id_barang')
-            ->join('barang', 'barang_masuk.id_barang = barang.id_barang')
-            ->join('kategori', 'barang.id_kategori = kategori.id_kategori')
-            ->join('ruang', 'barang_masuk.id_ruang = ruang.id_ruang')
-            ->groupBy('barang.nama_barang, ruang.nama_ruang')
-            ->get()->getResultArray();
-        // dd($hasil);
+        $db = \Config\Database::connect();
+        $sql = "SELECT B.nama_barang, kat.nama_kategori,
+        IF(K.jum IS NULL, M.jum, M.jum-K.jum) AS Jumlah,
+        R.nama_ruang
 
+        FROM
+        (SELECT id_barang, id_ruang, sum(jumlah_barang) AS jum
+        FROM barang_masuk
+        GROUP BY id_barang, id_ruang) M
+
+        LEFT JOIN
+
+        (SELECT id_barang, id_ruang, sum(jumlah_barang) AS jum
+        FROM barang_keluar
+        GROUP BY id_barang, id_ruang) K
+
+        ON M.id_barang = K.id_barang
+        AND M.id_ruang = K.id_ruang
+
+        JOIN barang B, kategori kat, ruang R
+        WHERE M.id_barang = B.id_barang
+        AND B.id_kategori = kat.id_kategori
+        AND M.id_ruang = R.id_ruang
+        GROUP BY M.id_barang, M.id_ruang;
+        ";
+        $q = $db->query($sql);
+        $hasil = $q->getResultArray();
 
         $data['barang'] = $hasil;
         return view('templates/data-barang', $data);
