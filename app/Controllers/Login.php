e3c3c9a5c4f0e5bf5a683548c1229a12a7d76647
Sorrecer <Anagakenny24@gmail.com>
2022-03-20 22:48:42 +0700

    login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,4 +8,23 @@ class Login extends BaseController
     {
         return view('templates/login');
     }
+
+    public function validasi()
+    {
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+        $valid = true;
+        // validasi input
+        if (!$this->validate([
+            'username' => ['label' => 'username', 'rules' => 'required'],
+            'password' => ['label' => 'password', 'rules' => 'required']
+        ])) {
+            // $validation = $this->validator;
+            return view('templates/tambah-kategori', [
+                'validation' => $this->validator
+            ]);
+        }
+
+        return $this->response->redirect(base_url('templates/index'));
+    }
 }
